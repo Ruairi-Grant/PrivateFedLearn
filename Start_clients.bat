3e9daafcac26bd65733c4e6bcd86d32a413ba9df
Ruairi-Grant <grantru@tcd.ie>
2024-02-18 13:51:02 +0000

    Improved bat files for client testing

--- Start_clients.bat
+++ Start_clients.bat
@@ -1,15 +1,18 @@
 @echo off
 title Clients
-set SCRIPT_DIR="C:\git_repos\Thesis"
-rem Number of times to run the script
-set NUM_RUNS=3
 
-rem Activate the flower env
-call flowerEnv\Scripts\activate.bat
 rem Number of times to run the script
 set NUM_RUNS=3
 
 rem Run the Python script multiple times in different terminals
 for /l %%i in (1,1,%NUM_RUNS%) do (
-    start cmd /k "cd %SCRIPT_DIR% && call flowerEnv\Scripts\activate.bat && python fl_client.py --cid=%%i --server_address "127.0.0.1:65432""
+    tasklist /FI "WINDOWTITLE eq Client %%i" | findstr /C:"cmd.exe" > nul
+    if not errorlevel 1 (
+        taskkill /FI "WINDOWTITLE eq Client %%i"
+    )
+    start cmd /k call client_setup.bat %%i
 )
+
+
+
+
---
+++ client_setup.bat
@@ -0,0 +1,7 @@
+set cid=%1
+set SCRIPT_DIR=C:\git_repos\Thesis
+
+title Client %1
+cd %SCRIPT_DIR%
+call flowerEnv\Scripts\activate.bat
+python fl_client.py  --server_address "127.0.0.1:65432" --cid %1
