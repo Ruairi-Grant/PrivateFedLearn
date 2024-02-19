a7038c56007f54cbc95010630bf115193489a254
Ruairi-Grant <grantru@tcd.ie>
2024-02-19 19:29:27 +0000

    Added metrics to centralized training

--- Centralized.py
+++ Centralized.py
@@ -1,11 +1,9 @@
-'''Script to train a model, with all of the data centralized'''
+"""Script to train a model, with all of the data centralized"""
 
-# General Utility functions
+# General Utility imports
 import os
 from pathlib import Path
-import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 
 # Tensorflow model import
 import tensorflow as tf
@@ -13,15 +11,20 @@ from keras import layers as tfkl
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 from keras.models import load_model
 
+# Matplotlib for visualization
+import matplotlib.pyplot as plt
+
+# Custom modules
 from MySqueezeNet import SqueezeNet
 import common
 
 # Defing Hyperparamaters
-EPOCHS = 40
+EPOCHS = 100
 BATCH_SIZE = 50
 SEED = 42
 IMAGE_SIZE = [265, 265]
 DATA_DIR = Path("Datasets\\aptos2019-blindness-detection\\train")
+RESULTS_DIR = Path("Results\\Centralized_DR")
 
 # create train and test datasets
 image_count = len(list(DATA_DIR.glob("*/*.png")))
@@ -32,6 +35,7 @@ list_ds = list_ds.shuffle(image_count, reshuffle_each_iteration=False)
 
 class_names = np.array(sorted([item.name for item in DATA_DIR.glob("*")]))
 
+# split the dataset into training and validation sets
 val_size = int(image_count * 0.2)
 train_ds = list_ds.skip(val_size)
 val_ds = list_ds.take(val_size)
@@ -39,31 +43,27 @@ val_ds = list_ds.take(val_size)
 print(f"Training data size: {tf.data.experimental.cardinality(train_ds).numpy()}")
 print(f"Validation data size: {tf.data.experimental.cardinality(val_ds).numpy()}")
 
-def get_class_count(num_classes, dataset):
-    '''Function to get the class count of the dataset'''
-    count = np.zeros(num_classes, dtype=np.int32)
-    for _, labels in dataset:
-        y, _, c = tf.unique_with_counts(labels)
-        count[y.numpy()] += c.numpy()
-    return count
-
-
+# map the image paths to the images and labels
 train_ds = train_ds.map(lambda x: common.process_path(x, class_names, IMAGE_SIZE))
 val_ds = val_ds.map(lambda x: common.process_path(x, class_names, IMAGE_SIZE))
 
 train_ds = common.configure_for_performance(train_ds, BATCH_SIZE)
 val_ds = common.configure_for_performance(val_ds, BATCH_SIZE)
 
-print(f"Training class distribution: {get_class_count(len(class_names), train_ds )}")
-print(f"Validation class distribution: {get_class_count(len(class_names), val_ds )}")
+print(
+    f"Training class distribution: {common.get_class_count(len(class_names), train_ds )}"
+)
+print(
+    f"Validation class distribution: {common.get_class_count(len(class_names), val_ds )}"
+)
 
+# Data augmentation
 data_prep = tf.keras.Sequential(
     [
         # tfkl.Rescaling(1./255),
         tfkl.CenterCrop(224, 224)
     ]
 )
-
 
 data_augmentation = tf.keras.Sequential([data_prep, tfkl.RandomFlip("horizontal")])
 
@@ -74,10 +74,10 @@ val_ds = val_ds.map(lambda x, y: (data_prep(x), y))
 # test_ds = test_ds.map(lambda x, y: (data_prep(x), y))
 
 print(
-    f"Post processing training class distribution: {get_class_count(len(class_names), train_ds )}"
+    f"Post processing training class distribution: {common.get_class_count(len(class_names), train_ds )}"
 )
 print(
-    f"Post processing Validation class distribution: {get_class_count(len(class_names), val_ds )}"
+    f"Post processing Validation class distribution: {common.get_class_count(len(class_names), val_ds )}"
 )
 
 # create the model
@@ -92,7 +92,6 @@ model.compile(
 )
 
 model.summary()
-
 
 # simple early stopping
 early_stopping = EarlyStopping(monitor="val_loss", mode="min", verbose=1, patience=20)
@@ -124,7 +123,7 @@ ax1.plot(epochs_range, acc, label="Training Accuracy")
 ax1.plot(epochs_range, val_acc, label="Validation Accuracy")
 ax1.legend(loc="lower right")
 
-fig1.savefig("Figures\\TrainingValidationAccuracy")
+fig1.savefig(os.path.join(RESULTS_DIR, "TrainingValidationAccuracy"))
 
 # PLot the dataset and save it
 fig2, ax2 = plt.subplots(figsize=(7, 5))
@@ -132,8 +131,8 @@ fig2, ax2 = plt.subplots(figsize=(7, 5))
 ax2.plot(epochs_range, loss, label="Training Loss")
 ax2.plot(epochs_range, val_loss, label="Validation Loss")
 ax2.legend(loc="lower right")
-fig2.savefig("Figures\\TrainingValidationLoss")
+fig2.savefig(os.path.join(RESULTS_DIR, "TrainingValidationLoss"))
 
-test_loss, test_acc = model.evaluate(train_ds, verbose=2)
-
-print("\nTest accuracy:", test_acc)
+# Evaluate the model
+common.evaluate_model(best_model, train_ds, os.path.join(RESULTS_DIR, "Train"))
+common.evaluate_model(best_model, val_ds, os.path.join(RESULTS_DIR, "Validation"))
--- common.py
+++ common.py
@@ -1,10 +1,20 @@
-'''Module to define common functions for the data processing and model training'''
+"""Module to define common functions for the data processing and model training"""
+
+# General imports
 import os
+import numpy as np
 import tensorflow as tf
+
+# SKlearn model evaluation
+from sklearn.metrics import confusion_matrix, classification_report
+
+# Matplotlib and seaborn for visualization
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 def configure_for_performance(ds, batch_size):
-    '''Function to configure the dataset for performance'''
+    """Function to configure the dataset for performance"""
     ds = ds.cache()
     ds = ds.shuffle(buffer_size=1000)
     ds = ds.batch(batch_size)
@@ -13,7 +23,7 @@ def configure_for_performance(ds, batch_size):
 
 
 def get_label(file_path, classes):
-    '''Function to get the label of the image from the file path'''
+    """Function to get the label of the image from the file path"""
     # Convert the path to a list of path components
     parts = tf.strings.split(file_path, os.path.sep)
     # The second to last is the class-directory
@@ -23,7 +33,7 @@ def get_label(file_path, classes):
 
 
 def decode_img(img, im_size):
-    '''Function to decode the image from the file path and resize it to the desired size'''
+    """Function to decode the image from the file path and resize it to the desired size"""
     # Convert the compressed string to a 3D uint8 tensor
     img = tf.io.decode_jpeg(img, channels=3)
     # Resize the image to the desired size
@@ -31,9 +41,47 @@ def decode_img(img, im_size):
 
 
 def process_path(file_path, classes, im_size):
-    '''General function to process the image'''
+    """General function to process the image"""
     label = get_label(file_path, classes)
     # Load the raw data from the file as a string
     img = tf.io.read_file(file_path)
     img = decode_img(img, im_size)
     return img, label
+
+
+def get_class_count(num_classes, dataset):
+    """Function to get the class count of the dataset"""
+    count = np.zeros(num_classes, dtype=np.int32)
+    for _, labels in dataset:
+        y, _, c = tf.unique_with_counts(labels)
+        count[y.numpy()] += c.numpy()
+    return count
+
+
+def evaluate_model(model, dataset, dir_path):
+    """Function to evaluate the model and save the confusion matrix and classification report"""
+    _, eval_acc = model.evaluate(dataset, verbose=2)
+    print("\nTrain accuracy:", eval_acc)
+    # Clear the current matplotlib figure
+    plt.clf()
+    # Get the true labels and predicted labels
+    true_labels = []
+    predicted_labels = []
+
+    for images, labels in dataset:
+        predictions = model.predict(images)
+        predicted_labels.extend(np.argmax(predictions, axis=1))
+        true_labels.extend(labels.numpy())
+
+    # Create the confusion matrix
+    cm = confusion_matrix(true_labels, predicted_labels)
+
+    # Print the confusion matrix using seaborn
+    sns.heatmap(cm, annot=True, fmt="d")
+    plt.savefig(dir_path + "/confusion_matrix.png")
+
+    # Create the classification report
+    report = classification_report(true_labels, predicted_labels)
+
+    with open(dir_path + "/classification_report.txt", "w") as file:
+        file.write(report)
